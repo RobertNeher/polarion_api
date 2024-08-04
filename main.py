8ceb7e8d6e402fe92d1364932a2e41690d245acc
rneher <robert.neher@bartscher-hasenaecker.de>
2024-08-04 17:35:00 +0200

    List of projects including qualifying callback

--- main.py
+++ main.py
@@ -4,18 +4,27 @@ from utilities.configuration import Configuration
 from view.project import ProjectView
 
 def main(page: ft.Page):
-    config = Configuration()
-    projectView = ProjectView()
+    def openProject(e):
+        print(f"callback: {e.control.data}")
 
-    page.add(controls=[
-        ft.Column(controls=[
-            ft.Text(f"Projects found in server {config.schema}://{config.polarionHost}",
+    config = Configuration()
+    projectView = ProjectView(callback=openProject)
+    projectCards = projectView.widgets
+
+    page.add(ft.Column(controls=[
+            ft.Text(f"Projects on {config.schema}://{config.polarionHost}",
                     text_align = ft.TextAlign.CENTER,
+                    size=30,
                     style=config.headerStyle
             ),
-            ft.GridView(controls=projectView.widgets)
+            ft.Container(bgcolor=ft.colors.DEEP_ORANGE_400),
+            ft.Column(controls=projectCards,
+                expand=0,
+                spacing=10,
+            )
         ])
-    ])
+    )
+    page.update()
 
 
 
--- view/project.py
+++ view/project.py
@@ -4,15 +4,20 @@ from utilities.configuration import Configuration
 from model.project import Project
 
 class ProjectView():
-    def __init__(self) -> None:
+    def __init__(self, callback) -> None:
+        config = Configuration()
         self.widgets = []
         _projects = Project()
-        self.projects = _projects.projects
+        projects = _projects.projects
 
-        for _project in self.projects:
-            config = Configuration()
+        for _project in projects:
             projectDetail = _projects.getDetails(projectID=_project["id"])
-            print(projectDetail["description"])
+
+            if "description" in projectDetail.keys():
+                description = projectDetail["description"]["value"]
+            else:
+                description = "<empty>"
+
             self.widgets.append(
                 ft.Card(
                     content=ft.Container(
@@ -20,21 +25,27 @@ class ProjectView():
                                 ft.ListTile(
                                     leading=ft.Icon(ft.icons.ALBUM),
 
-                                    title=ft.Text(f"{projectDetail['name']} (ID: {projectDetail['id']}",
+                                    title=ft.Text(f"{projectDetail['name']} (ID: {projectDetail['id']})",
                                         style=config.headerStyle
                                     ),
                                     subtitle=ft.Text(
-                                        projectDetail["description"]["value"],
+                                        description,
                                         style=config.normalStyle
                                     ),
                                 ),
                                 ft.Row(
-                                    controls=[ft.TextButton("Open")],
+                                    controls=[ft.TextButton(
+                                        "Open",
+                                        on_click=callback,
+                                        data=_project["id"])
+                                    ],
                                     alignment=ft.MainAxisAlignment.END,
                                 )
                             ]
                         )
                     ),
                     width=400,
+                    height=150,
+                    color=ft.colors.BLUE_GREY_100,
                 )
             )
